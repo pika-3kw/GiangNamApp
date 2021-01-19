966c88f45af175251538924481ec2cc8d6cb79c5
GiangNam <giangnam10a2@gmail.com>
2021-01-19 19:23:50 +0700

    initial commit

---
+++ apps/nam-dep-trai/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  displayName: 'nam-dep-trai',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/nam-dep-trai',
+};
